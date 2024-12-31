cd0d4f6b25825ca55351c4ac7141cee25af99cdc
montanius <gmacanovic@gmail.com>
2024-12-31 13:51:03 +0100

    Kreirana ruta za update  podataka za korisnika.

--- routes/users.js
+++ routes/users.js
@@ -26,4 +26,24 @@ return res.status(500).json({ message : 'Došlo je do greške'});
 }
     });
 
+    router.patch('/update', provjeriToken, async (req, res) => {
+        const userId = req.user.id;
+const [field, value] = Object.entries(req.body)[0];
+
+ try{
+const updateData = {[field]: value};
+
+const result = await  user.updateOne({_id: userId}, {$set: updateData});
+
+if(result.modifiedCount === 0){
+return res.status(400).json({message: 'Podatak nije izmijenjen ili korisnik nije pronađen.'});
+}
+
+    return res.status(200).json({message: 'Podatak je ažuriran.'});
+ }
+ catch(error){
+return res.status(500).json({message: 'Greška pri ažuriranju podataka.', error:error.message});
+ }
+    });
+    
 module.exports = router;
